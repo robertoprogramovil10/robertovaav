67164b61b519da587f5d9d7e579c23133eb7d2e5
robertoprogramovil10 <robertoprogramovil10@users.noreply.github.com>
2015-10-20 23:39:46 -0500

    Update App.cs

--- Actividad4/Actividad4/App.cs
+++ Actividad4/Actividad4/App.cs
@@ -1,33 +1,23 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
 
 namespace Actividad4
 {
-	public class App
-	{
-		public static Page GetMainPage ()
-		{	
-			//Se crea un objeto etiqueta
-			Label label = new Label();
-
-			//Se modifican sus propiedades
-			//Texto
-			label.Text = "Cámbiame";
-			//Color del texto
-			label.TextColor = Color.Blue;
-			//Alineación vertical (YAlign). Para la alineación horizontal, usar XAlign
-			label.YAlign = TextAlignment.Center;
-
-
-			//El mismo código usando inicializador de objetos
-			//Label label = new Label{ Text = "Cámbiame", TextColor =  Color.Blue, YAlign = TextAlignment.Center };
-
-			//Se crea una página y se le asigna como contenido la eqtiqueta que se creó
-			ContentPage contentPage = new ContentPage();
-			contentPage.Content = label;
-
-			return contentPage;
-		}
-	}
+    public class App
+    {
+        public static Page GetMainPage ()
+        {    
+            return new ContentPage {
+                Content = new Label {
+                    Text = "Cambiando propiedades por inicializadores de objetos",
+                    Text = "Roberto Vazquez Avila",
+                    BackgroundColor = Color.Red,
+                    TextColor = Color.blue,
+                    VerticalOptions = LayuoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
+                },
+            };    
+        }    
+    }
 }
 
