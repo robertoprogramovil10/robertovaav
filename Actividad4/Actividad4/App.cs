d863da8b280d86c936e4212bb251d3409b0fac1a
robertoprogramovil10 <robertovaav@msn.com>
2015-11-29 17:48:27 -0600

    Utilizando  inicializadores de objetos en lugar de propiedades

--- Actividad4/Actividad4/App.cs
+++ Actividad4/Actividad4/App.cs
@@ -3,21 +3,20 @@ using Xamarin.Forms;
 
 namespace Actividad4
 {
-    public class App
-    {
-        public static Page GetMainPage ()
-        {    
-            return new ContentPage {
-                Content = new Label {
-                    Text = "Cambiando propiedades por inicializadores de objetos",
-                    Text = "Roberto Vazquez Avila",
-                    BackgroundColor = Color.Red,
-                    TextColor = Color.blue,
-                    VerticalOptions = LayuoutOptions.Center,
-                    HorizontalOptions = LayoutOptions.Center,
-                },
-            };    
-        }    
-    }
+	public class App
+	{
+		public static Page GetMainPage ()
+		{    
+			return new ContentPage {
+				Content = new Label {
+					Text = "Cambiando propiedades por inicializadores de objetos",
+					Text = "Roberto Vazquez Avila",
+					BackgroundColor = Color.Red,
+					TextColor = Color.blue,
+					VerticalOptions = LayuoutOptions.Center,
+					HorizontalOptions = LayoutOptions.Center,
+				},
+			};    
+		}    
+	}
 }
-
